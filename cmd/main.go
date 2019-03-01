0a64d78219b23b2913a2a16d1958c08f0bc5f99e
Scott Nichols <nicholss@google.com>
2019-03-01 14:50:08 -0800

    Integrate CloudEvents SDK.

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/cloudevents/sdk-go/pkg/cloudevents/client"
 	"github.com/mchmarny/knative-ws-example/pkg/handlers"
 	"github.com/mchmarny/knative-ws-example/pkg/utils"
 
@@ -12,30 +15,41 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
+
+	// Server configured
+	port, err := strconv.Atoi(utils.MustGetEnv("PORT", "8080"))
+	if err != nil {
+		log.Fatalf("failed to parse port to int, %s", err.Error())
+	}
 
 	// init configs
 	handlers.InitHandlers()
 
 	// Static
 	http.Handle("/static/", http.StripPrefix("/static/",
-		  http.FileServer(http.Dir("static"))))
+		http.FileServer(http.Dir("static"))))
 
 	// UI Handlers
 	http.HandleFunc("/", handlers.RootHandler)
 	http.Handle("/ws", websocket.Handler(handlers.WSHandler))
 
 	// Ingres API Handler
-	http.HandleFunc("/v1/event", handlers.CloudEventHandler)
+	c, err := client.NewHTTPClient(client.WithHTTPPath("/v1/event"))
+	if err != nil {
+		log.Fatalf("failed to create cloudevents client, %s", err.Error())
+	}
 
 	// Health Handler
 	http.HandleFunc("/_health", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, "ok")
 	})
 
-	// Server configured
-	port := utils.MustGetEnv("PORT", "8080")
+	if err := c.StartReceiver(ctx, handlers.CloudEventReceived); err != nil {
+		log.Fatalf("failed to start cloudevents receiver, %s", err.Error())
+	}
+	log.Printf("Server starting on port %d \n", port)
 
-	log.Printf("Server starting on port %s \n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-
+	// Block until done.
+	<-ctx.Done()
 }
